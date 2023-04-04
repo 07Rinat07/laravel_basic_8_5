da420279b347c0670145bddfeb0e33e58768c598
Rinat Sarmuldin <ura07srr@gmail.com>
2023-04-04 19:37:33 +0500

    Laravel course. Filter Pattern. Filtering Data in Laravel. lesson 30.

--- app/Http/Controllers/Post/IndexController.php
+++ app/Http/Controllers/Post/IndexController.php
@@ -3,13 +3,23 @@
 namespace App\Http\Controllers\Post;
 
 use App\Http\Controllers\Controller;
+use App\Http\Filters\PostFilter;
+use App\Http\Requests\Post\FilterRequest;
 use App\Models\Post;
 
 class IndexController extends BaseController
 {
-  public function __invoke()
-  {
-      $posts = Post::paginate(10);
+    public function __invoke(FilterRequest $request)
+    {
+        $data = $request->validated ();
+
+        $query = Post::query ();
+
+        $data = $request->validated ();
+        $filter = app ()->make (PostFilter::class, ['queryParams' => array_filter ($data)]);
+        $posts = Post::filter ($filter)->paginate(10);
+//        dd ($posts);
+
       return view ('post.index', compact ('posts'));
-  }
+    }
 }
